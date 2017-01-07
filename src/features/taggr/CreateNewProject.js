276e21bc39666e03f8d49283499bddd4c0dd9ab2
David Higgins <thehig@gmail.com>
2017-01-07 14:13:37 +0000

    Insert the Create New Project component design

--- src/features/taggr/CreateNewProject.js
+++ src/features/taggr/CreateNewProject.js
@@ -7,8 +7,16 @@ export default class CreateNewProject extends PureComponent {
 
   render() {
     return (
-      <div className="taggr-create-new-project">
-        Component content: taggr/CreateNewProject
+      <div className="taggr-create-new-project col">
+          <section className="min-height section-1 dbg-color-1">
+              <label for="add-new-project">Create new Project</label>
+              <div className="input-group">
+                  <input id="add-new-project" type="text" className="form-control" placeholder="New project name" />
+                  <span className="input-group-btn">
+                      <button className="btn btn-default" type="button"><span class="glyphicon glyphicon-plus"></span> Add</button>
+                  </span>
+              </div>
+          </section>
       </div>
     );
   }
