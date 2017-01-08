69188582e93342a3a855a964ef159ff07c610971
David Higgins <thehig@gmail.com>
2017-01-08 15:49:33 +0000

    Wrap CreateNewProject inside a HTML form

--- src/features/taggr/CreateNewProject.js
+++ src/features/taggr/CreateNewProject.js
@@ -11,23 +11,38 @@ export default class CreateNewProject extends PureComponent {
   }
 
   createProjectClick(evt){
+    evt.preventDefault();
+    if(!this.input.value) return;
+
     this.props.createProject(this.input.value);
     this.input.value = "";
     this.input.focus();
   }
 
-  render() {
-    return (
-      <div className="taggr-create-new-project col">
-          <section className="min-height section-1 dbg-color-1">
-              <label htmlFor="add-new-project">Create new Project</label>
-              <div className="input-group">
-                  <input id="add-new-project" ref={(input)=> {this.input = input;}} type="text" className="form-control" placeholder="New project name" />
-                  <span className="input-group-btn">
-                      <button className="btn btn-default" type="button" onClick={this.createProjectClick}><span className="glyphicon glyphicon-plus"></span> Add</button>
-                  </span>
-              </div>
-          </section>
+  render(){
+    return(
+      <div className="taggr-create-new-project">
+        <section className="min-height section-1 dbg-color-1">
+
+        <form className="form-horizontal">
+        <fieldset>
+
+        <legend>Create New Project</legend>
+
+        <div className="form-group">
+          <label className="control-label" htmlFor="appendedtext">Name</label>
+            <div className="input-group">
+              <input id="appendedtext" ref={(input)=> {this.input = input;}} name="appendedtext" className="form-control" placeholder="" type="text"/>
+              <span className="input-group-btn">
+                  <button className="btn btn-default" type="submit" onClick={this.createProjectClick}><span className="glyphicon glyphicon-plus"></span> Add</button>
+              </span>
+          </div>
+        </div>
+        </fieldset>
+        </form>
+
+
+        </section>
       </div>
     );
   }
